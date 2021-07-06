4b2c7f93fd50ad370fe5136d98997c7dee3485c7
Tadeusz de Ruijter <tadeuszderuijter@gmail.com>
2021-07-06 21:23:54 +0200

    project upload

---
+++ components/blocks/logo.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="brand__logo">
+        <img src="~assets/img/soextranails.svg" width="100px" height="120px" alt="So Extra Nails" title="So Extra Nails">
+    </div>
+</template>
