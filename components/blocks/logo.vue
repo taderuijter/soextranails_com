ee875d65462f9c86f1fbe99bfbd912ae6974faa0
Tadeusz de Ruijter <tadeuszderuijter@gmail.com>
2021-11-12 23:39:18 +0100

    Delete Project

--- components/blocks/logo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="brand__logo">
-        <img src="~assets/img/soextranails.svg" width="100px" height="120px" alt="So Extra Nails" title="So Extra Nails">
-    </div>
-</template>
